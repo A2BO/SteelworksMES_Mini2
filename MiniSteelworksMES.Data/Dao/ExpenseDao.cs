96292db6d90d6814486064d48f4e0cb8bc48e71c
WooJae Kang <wj.kang12@gmail.com>
2020-12-16 18:22:24 +0900

    데이터베이스

--- MiniSteelworksMES.Data/Dao/ExpenseDao.cs
+++ MiniSteelworksMES.Data/Dao/ExpenseDao.cs
@@ -18,67 +18,67 @@ namespace MiniSteelworksMES.Data.Dao
             return x => x.ExpenseId == key;
         }
 
-        //public List<Expense> Search()
+        public List<Expense> Search()
+        {
+            using (MesEntities context = new MesEntities())
+            {
+                var query = from x in context.Expenses
+                            select new
+                            {
+                                Expense = x,
+                                CategoryName = x.ExpenseLine.Name
+                            };
+
+                var list = query.ToList();
+                foreach(var item in list)
+                {
+                    item.Expense.CategoryName = item.CategoryName;
+                }
+
+                return list.Select(x => x.Expense).ToList();
+            }
+        }
+
+        //public List<Expense> Search2()
         //{
         //    using (MesEntities context = new MesEntities())
         //    {
+        //        var lines = context.ExpenseLines.ToList();
+
         //        var query = from x in context.Expenses
-        //                    select new
-        //                    {
-        //                        Expense = x,
-        //                        CategoryName = x.ExpenseLine.Name
-        //                    };
+        //                    select x;
 
         //        var list = query.ToList();
-        //        foreach(var item in list)
+        //        foreach (var x in list)
         //        {
-        //            item.Expense.CategoryName = item.CategoryName;
+        //            ExpenseLine line = lines.Find(l => l.ExpenseLineId == x.ExpenseLineId);
+        //            x.CategoryName = line.Name;
         //        }
 
-        //        return list.Select(x => x.Expense).ToList();
+        //        return list;
         //    }
         //}
 
-        //public List<Expense> Search2()
-        {
-            using (MesEntities context = new MesEntities())
-            {
-                var lines = context.ExpenseLines.ToList();
+        //public List<Expense> Search3()
+        //{
+        //    using (MesEntities context = new MesEntities())
+        //    {
+        //        Dictionary<int, string> categoryNames = context.ExpenseLines.ToDictionary(
+        //            x => x.ExpenseLineId,
+        //            x => x.Name);
 
-                var query = from x in context.Expenses
-                            select x;
+        //        var query = from x in context.Expenses
+        //                    select x;
 
-                var list = query.ToList();
-                foreach (var x in list)
-                {
-                    ExpenseLine line = lines.Find(l => l.ExpenseLineId == x.ExpenseLineId);
-                    x.CategoryName = line.Name;
-                }
+        //        var list = query.ToList();
+        //        foreach (var x in list)
+        //        {
+        //            x.CategoryName = categoryNames[x.ExpenseLineId];
+        //        }
 
-                return list;
-            }
-        }
-
-        public List<Expense> Search3()
-        {
-            using (MesEntities context = new MesEntities())
-            {
-                Dictionary<int, string> categoryNames = context.ExpenseLines.ToDictionary(
-                    x => x.ExpenseLineId,
-                    x => x.Name);
-
-                var query = from x in context.Expenses
-                            select x;
-
-                var list = query.ToList();
-                foreach (var x in list)
-                {
-                    x.CategoryName = categoryNames[x.ExpenseLineId];
-                }
-
-                return list;
-            }
-        }
+        //        return list;
+        //    }
+        //}
 
         public List<Expense> GetAll()
         {
