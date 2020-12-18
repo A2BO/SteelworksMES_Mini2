008648b3d8cf0e5055f6574727bec9b79d417059
Yoon kitaek <awesomejooyoung@gmail.com>
2020-12-18 13:58:23 +0900

    영업실적 그래프 작성중

--- MesUI/Mini2/SalesPerformence.Designer.cs
+++ MesUI/Mini2/SalesPerformence.Designer.cs
@@ -30,11 +30,71 @@ namespace MesUI
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            DevExpress.XtraCharts.XYDiagram xyDiagram1 = new DevExpress.XtraCharts.XYDiagram();
+            DevExpress.XtraCharts.Series series1 = new DevExpress.XtraCharts.Series();
+            DevExpress.XtraCharts.Series series2 = new DevExpress.XtraCharts.Series();
+            this.teamBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.chartControl1 = new DevExpress.XtraCharts.ChartControl();
+            this.saleBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.teamBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chartControl1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(xyDiagram1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(series1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(series2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.saleBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // teamBindingSource
+            // 
+            this.teamBindingSource.DataSource = typeof(MiniSteelworksMES.Data.Team);
+            // 
+            // chartControl1
+            // 
+            this.chartControl1.DataSource = this.saleBindingSource;
+            xyDiagram1.AxisX.VisibleInPanesSerializable = "-1";
+            xyDiagram1.AxisY.VisibleInPanesSerializable = "-1";
+            this.chartControl1.Diagram = xyDiagram1;
+            this.chartControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.chartControl1.Location = new System.Drawing.Point(0, 0);
+            this.chartControl1.Name = "chartControl1";
+            series1.ArgumentDataMember = "Name";
+            series1.DataSource = this.teamBindingSource;
+            series1.Name = "목표건수";
+            series1.ValueDataMembersSerializable = "Target";
+            series2.Name = "달성건수";
+            this.chartControl1.SeriesSerializable = new DevExpress.XtraCharts.Series[] {
+        series1,
+        series2};
+            this.chartControl1.Size = new System.Drawing.Size(800, 525);
+            this.chartControl1.TabIndex = 0;
+            this.chartControl1.Click += new System.EventHandler(this.chartControl1_Click);
+            // 
+            // saleBindingSource
+            // 
+            this.saleBindingSource.DataSource = typeof(MiniSteelworksMES.Data.Sale);
+            // 
+            // SalesPerformence
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 14F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(800, 525);
+            this.Controls.Add(this.chartControl1);
+            this.Name = "SalesPerformence";
             this.Text = "SalesPerformence";
+            ((System.ComponentModel.ISupportInitialize)(this.teamBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(xyDiagram1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(series1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(series2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chartControl1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.saleBindingSource)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private DevExpress.XtraCharts.ChartControl chartControl1;
+        private System.Windows.Forms.BindingSource saleBindingSource;
+        private System.Windows.Forms.BindingSource teamBindingSource;
     }
 }
